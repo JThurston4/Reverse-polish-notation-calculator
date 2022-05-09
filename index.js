2586c3dfb2a3ac4f18cb22147ac21584a691c23f
Josh <joshthurston92@gmail.com>
2022-05-09 03:37:54 -0600

    need to research how to test against cli

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ function isOperation(answer) {
   return operations.some((operation) => operation === answer);
 }
 
-let answers = [0, 0];
-while (true) {
-  const answer = readlineSync.prompt();
+const calculator = () => {
 
-  if (isNaN(Number(answer)) && !isOperation(answer)) {
-    console.log('Invalid input, number or operation required')
-  } else if (isOperation(answer)){
-    console.log(evaluate(`${answers[answers.length-1]} ${answer} ${answers[answers.length-2]}`))
-  } else {
-    answers.push(answer);
+  let answers = [0, 0];
+  while (true) {
+    const answer = readlineSync.prompt();
+
+    if (isNaN(Number(answer)) && !isOperation(answer)) {
+      console.log('Invalid input, number or operation required')
+    } else if (isOperation(answer)){
+      console.log(evaluate(`${answers[answers.length-1]} ${answer} ${answers[answers.length-2]}`))
+    } else {
+      answers.push(answer);
+    }
   }
 }
+
+module.exports = calculator;
